406995827a1ee9a34101418a623e23b2a44df302
mkheck13 <mkheck13@gmail.com>
2024-05-09 10:43:29 -0700

    kl

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import React from "react";
-import NavbarComponent from "./Components/NavbarComponent";
-import SkillsComponent from "./Components/SkillsComponent";
-import AboutMeComponent from "./Components/AboutMeComponent";
-import ProjectsComponent from "./Components/ProjectsComponent";
-import FooterComponent from "./Components/FooterComponent";
+import NavbarComponent from "../Components/NavbarComponent";
+import SkillsComponent from "../Components/SkillsComponent";
+import AboutMeComponent from "../Components/AboutMeComponent";
+import ProjectsComponent from "../Components/ProjectsComponent";
+import FooterComponent from "../Components/FooterComponent";
 
 const Home = () => {
   return (
