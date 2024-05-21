04376fc304a8d0dd418b02d0b3c492a3c20b6a4a
mkheck13 <mkheck13@gmail.com>
2024-05-21 08:33:19 -0700

    Projects updated

--- src/Components/ProjectsComponent.tsx
+++ src/Components/ProjectsComponent.tsx
@@ -3,10 +3,10 @@
 
 import React from 'react'
 import Image from "next/image";
-import dash from '../Assets/dash.png'
+
 import advice from '../Assets/advice.png'
 import pokelog from '../Assets/pokelog.png'
-import tipclac from '../Assets/tipcalc.png'
+
 import caddyTrack from '../Assets/caddyTrack.png'
 
 const ProjectsComponent = () => {
@@ -15,75 +15,52 @@ const ProjectsComponent = () => {
      <h1 id="projects" className="text-center pt-12 fontlife font-bold text-4xl text-teal-500">Projects</h1>
      <br />
 
-     <div className='mx-60'>
+     <div className='flex grid-flow-row-dense  justify-between mx-6'>
 
-     <div className="carousel w-full">
-
-      
-  <div id="slide1" className="carousel-item relative w-full">  
-  <a href="https://socialmediadashboard-sage.vercel.app/" target='_blank'>
-    <Image src={dash} className='w-full' alt="social media dashboard"/>
-  </a>
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide5" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-
-
-  <div id="slide2" className="carousel-item relative w-full">
-    <a href="https://advicegentwo.vercel.app/" target='_blank'>
-      <Image src={advice} className='w-full' alt="social media dashboard"/>
-    </a>
-    
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-
-  <div id="slide3" className="carousel-item relative w-full">
-  <a href="https://pokerebuild.vercel.app/" target='_blank'>
-      <Image src={pokelog} className='w-full' alt="social media dashboard"/>
-    </a>
-    
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-
-  <div id="slide4" className="carousel-item relative w-full">
-  <a href="https://tipcalculator-kappa.vercel.app/" target='_blank'>
-      <Image src={tipclac} className='w-full' alt="social media dashboard"/>
-    </a>
-   
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide5" className="btn btn-circle">❯</a>
+     <div className="card w-96 bg-base-100 shadow-xl">
+  <figure><Image src={caddyTrack} className='w-full' alt="full stack app"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">Caddy Track</h2>
+    <p>Group full stack project</p>
+    <div className="card-actions justify-end">
+    <a href="https://caddytrackfrontend.vercel.app/" target='_blank'>
+    <button className="btn btn-primary" >Go To Site</button>
+    </a> 
     </div>
   </div>
+</div>
 
-  <div id="slide5" className="carousel-item relative w-full">
-<a href="https://caddytrackfrontend.vercel.app/" target='_blank'>
-      <Image src={caddyTrack} className='w-full' alt="social media dashboard"/>
-    </a>
-   
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
+<div className="card w-96 bg-base-100 shadow-xl">
+  <figure><Image src={pokelog} className='w-full' alt="pokemon project" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">PokeLog</h2>
+    <p>Pokemon Api Project</p>
+    <div className="card-actions justify-end">
+    <a href="https://pokerebuild.vercel.app/" target='_blank'>
+    <button className="btn btn-primary">Go To Site</button>
+      </a>
     </div>
   </div>
-
-
-
 </div>
 
 
+  
 
 
+<div className="card w-96 bg-base-100 shadow-xl">
+  <figure><Image src={advice} className='w-full' alt="advice generator"/></figure>
+  <div className="card-body">
+    <h2 className="card-title">Advice Generator</h2>
+    <p>Advice generator project</p>
+    <div className="card-actions justify-end">
+    <a href="https://advicegentwo.vercel.app/" target='_blank'>
+    <button className="btn btn-primary">Go To Site</button>
+      
+      </a>
 
-
+    </div>
+  </div>
+</div>
 
 
      </div>
